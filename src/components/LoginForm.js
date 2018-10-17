38702f5a9369c68240b7a1b28032b40644539ed6
NewtonJ <newtonjoaquim12@gmail.com>
2018-10-17 12:13:08 -0300

    Terminada LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Text, View} from 'react-native';
 import {connect} from 'react-redux';
 import {emailChanged, passwordChanged, loginUser} from '../actions'
-import {Card, CardSection, Input, Button} from './common';
+import {Card, CardSection, Input, Button, Spinner} from './common';
 
 
 class LoginForm extends React.Component{
@@ -16,6 +17,25 @@ class LoginForm extends React.Component{
     onButtonPressed(){
         const {email,password} = this.props
         this.props.loginUser({email, password})
+    }
+
+    renderError(){
+        if(this.props.error){
+            return(
+                <View style={{backgroundColor:'white'}}>
+                    <Text style={styles.errorTextStyle}>{this.props.error}</Text>
+                </View>
+            )
+        }
+    }
+
+    renderButton(){
+        if(this.props.loading){
+            return <Spinner size='large'/>
+        }
+        else{
+            return <Button onPress={this.onButtonPressed.bind(this)}>Login</Button>
+        }
     }
 
     render(){
@@ -40,18 +60,31 @@ class LoginForm extends React.Component{
                     />
                 </CardSection>
                 
+                {this.renderError()}
+
                 <CardSection>
-                    <Button onPress={this.onButtonPressed.bind(this)}>Login</Button>
+                    {this.renderButton()}
                 </CardSection>
+                
             </Card>
         );
+    }
+}
+
+const styles={
+    errorTextStyle:{
+        fontSize:20,
+        alignSelf:'center',
+        color:'red'
     }
 }
 
 const mapStateToProps = state => {
     return{
         email:state.auth.email,
-        password:state.auth.password
+        password:state.auth.password,
+        error:state.auth.error,
+        loading:state.auth.loading
     }
 };
 
