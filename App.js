e0ec42ef542ae18774be89e21e44ba04671f4ad5
NewtonJ <newtonjoaquim12@gmail.com>
2018-10-17 23:25:04 -0300

    Feito formulario para criacao de empregado

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {createStore, applyMiddleware} from 'redux';
 import firebase from '@firebase/app';
 import ReduxThunk from 'redux-thunk';
 import reducers from './src/reducers';
-import LoginForm from './src/components/LoginForm';
+import Router from './src/router';
 
 export default class App extends Component{
   componentWillMount(){
@@ -22,7 +22,7 @@ export default class App extends Component{
   render() {
     return (
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-        <LoginForm/>
+        <Router />
       </Provider>
     );
   }
---
+++ src/components/EmployeeList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default class EmployeeList extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+                <Text>EmployeeList</Text>
+            </View>
+        );
+    }
+}
