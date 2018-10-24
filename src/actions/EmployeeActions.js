88a02ef32c37633d101c178504c72b11ef15e076
NewtonJ <newtonjoaquim12@gmail.com>
2018-10-23 21:58:36 -0300

    Terminada conexao com data store do firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,8 @@
 import firebase from '@firebase/app';
 import '@firebase/auth';
 import '@firebase/database';
-import {EMPLOYEE_UPDATE} from './types'
+import {EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS} from './types'
+import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({prop, value}) => {
     return {
@@ -12,6 +13,24 @@ export const employeeUpdate = ({prop, value}) => {
 
 export const employeeCreate = ({name, phone, shift}) => {
     const {currentUser} = firebase.auth()
-    firebase.database().ref(`users/${currentUser.uid}/employees`)
-        .push({name, phone, shift});
+
+    return (dispatch) => {
+        firebase.database().ref(`users/${currentUser.uid}/employees`)
+            .push({name, phone, shift})
+            .then(() => {
+                dispatch({type:EMPLOYEE_CREATE});
+                Actions.pop()
+            });
+    }
+}
+
+export const employeesFetch = () => {
+    const {currentUser} = firebase.auth();
+
+    return(dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({type: EMPLOYEES_FETCH_SUCCESS, payload:snapshot.val()})
+            })
+    }
 }
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,19 +1,55 @@
+import _ from 'lodash';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {connect} from 'react-redux';
+import {ListView} from 'react-native';
+import {employeesFetch} from '../actions';
 
-export default class EmployeeList extends React.Component{
+import ListItem from './ListItem';
+
+class EmployeeList extends React.Component{
+
+    componentWillMount(){
+        this.props.employeesFetch();
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps){
+        //nextProps are the next set of Props that the component will receive
+        //this.props are the current props
+
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({employees}){
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
+
+        this.dataSource = ds.cloneWithRows(employees);
+    }
+
+    renderRow(employee){
+        return <ListItem employee={employee}/>;
+    }
+
     render(){
+        console.log(this.props)
         return(
-            <View>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-            </View>
+            <ListView
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         );
     }
 }
+
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return {...val, uid}; //{shift:"...", name:"...", id:"..."}
+    });
+
+    return {employees};
+}
+
+export default connect(mapStateToProps, {employeesFetch}) (EmployeeList);
